4b7297d6ac8ce183b44b969a49e08bd30b1f70ff
Charlsy <imcharlsy@gmail.com>
2022-03-24 22:50:13 -0700

    Solve FOUC by having styled component rendered on server side

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,19 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
 
 class MyDocument extends Document {
+  static getInitialProps ({ renderPage }) {
+    const sheet = new ServerStyleSheet()
+    const page = renderPage(App => props => sheet.collectStyles(<App {...props} />))
+    const styleTags = sheet.getStyleElement()
+    return { ...page, styleTags }
+  }
+
   render() {
     return (
       <Html>
         <Head>
+          {this.props.styleTags}
           <link
             href="https://fonts.googleapis.com/css2?family=Newsreader&display=swap"
             rel="stylesheet"
@@ -15,7 +24,7 @@ class MyDocument extends Document {
             as="font"
             type="font/woff2"
             crossOrigin="anonymous"
-          />      
+          />
         </Head>
         <body>
           <Main />
