8ed4ae52f2870f5db43606937d822798ebba0356
Charlsy <imcharlsy@gmail.com>
2022-03-24 23:05:59 -0700

    Fix FOUC (fingers crossed)

--- pages/_document.js
+++ pages/_document.js
@@ -2,18 +2,36 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
 class MyDocument extends Document {
-  static getInitialProps ({ renderPage }) {
+  static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheet()
-    const page = renderPage(App => props => sheet.collectStyles(<App {...props} />))
-    const styleTags = sheet.getStyleElement()
-    return { ...page, styleTags }
+    const originalRenderPage = ctx.renderPage
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        })
+
+      const initialProps = await Document.getInitialProps(ctx)
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      }
+    } finally {
+      sheet.seal()
+    }
   }
 
   render() {
     return (
       <Html>
         <Head>
-          {this.props.styleTags}
           <link
             href="https://fonts.googleapis.com/css2?family=Newsreader&display=swap"
             rel="stylesheet"
