1b18ab39289d847b499e25c1e3e9315d158f250a
Charlsy <imcharlsy@gmail.com>
2023-01-02 16:54:34 -0800

    Remove @next/font

--- pages/_app.js
+++ pages/_app.js
@@ -1,33 +1,14 @@
 import GlobalStyles from '../components/GlobalStyles'
 import { ThemeProvider } from 'next-themes'
-import localFont from '@next/font/local'
-
-const myFont = localFont({
-    src: [
-        {
-            path: '../public/fonts/ABCDiatypeVariable.woff2',
-        },
-        {
-            path: '../public/fonts/Newsreader.woff2',
-            weight: '400',
-            style: 'normal',
-        },
-        {
-            path: '../public/fonts/Newsreader-Italic.woff2',
-            weight: '400',
-            style: 'italic',
-        },
-    ],
-})
 
 function MyApp({ Component, pageProps }) {
     return (
-        <main className={myFont.className}>
+        <>
             <GlobalStyles />
             <ThemeProvider>
                 <Component {...pageProps} />
             </ThemeProvider>
-        </main>
+        </>
     )
   }
   
