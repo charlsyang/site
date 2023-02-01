1ea8f4c8beafeab232ca037d92fed3768b6e752d
Charlsy <imcharlsy@gmail.com>
2023-01-31 23:52:05 -0800

    Remove extra footer on blog post

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -47,7 +47,6 @@ export default function BlogPost({ code, frontmatter }) {
                     </Article>
                 </Main>
             </MaxWidthWrapper>
-            <Footer/>
         </>
     )
 }
