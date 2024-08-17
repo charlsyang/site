5fa995475224fe221a586664e5be8cfe33b116ad
Charlsy <imcharlsy@gmail.com>
2024-08-17 11:03:22 -0700

    Update /work

--- pages/work/index.js
+++ pages/work/index.js
@@ -44,8 +44,7 @@ export default function WorkIndex({ allPostsData }) {
             </Headline>
             <Callout>
               <p>
-                Charlsy is actively looking for full-time product
-                design roles. Learn more about{' '}
+                Learn more about{' '}
                 <Link href='https://charlsy.notion.site/Charlsy-as-a-Designer-d60da6d266e3419b861e422a6a910a80'>
                   Charlsy as a designer
                 </Link>
@@ -65,40 +64,6 @@ export default function WorkIndex({ allPostsData }) {
               </Link>
             </div>
           </ContactLinks>
-          <Experience>
-            <TableTitle>Experience</TableTitle>
-            <Table>
-              <tbody>
-                {' '}
-                {/* Added to avoid hydration failed error (caused by Next.js) */}
-                <TableRow>
-                  <TdYear>NOW</TdYear>
-                  <TdCompany>Microsoft</TdCompany>
-                  <TdRole>Product Designer</TdRole>
-                </TableRow>
-                <TableRow>
-                  <TdYear>2022</TdYear>
-                  <TdCompany>Rubrik</TdCompany>
-                  <TdRole>Product Design Intern</TdRole>
-                </TableRow>
-                <TableRow>
-                  <TdYear>2021</TdYear>
-                  <TdCompany>IDEO</TdCompany>
-                  <TdRole>Interaction Design Intern</TdRole>
-                </TableRow>
-                <TableRow>
-                  <TdYear>2020</TdYear>
-                  <TdCompany>Tide</TdCompany>
-                  <TdRole>Product Design Intern</TdRole>
-                </TableRow>
-                <TableRow>
-                  <TdYear>2019</TdYear>
-                  <TdCompany>NIO</TdCompany>
-                  <TdRole>UI/UX Design Intern</TdRole>
-                </TableRow>
-              </tbody>
-            </Table>
-          </Experience>
         </GridWrapper>
         <Projects>
           <SectionHeading>Projects</SectionHeading>
@@ -200,9 +165,9 @@ const Callout = styled.div`
 `;
 
 const ContactLinks = styled.div`
-  grid-column: 7 /9;
-  display: flex;
-  flex-direction: column;
+  grid-column: 7 / -1;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
   gap: var(--spacing-3x);
   font-weight: var(--font-weight-normal);
   animation: onload-fade var(--duration-load) var(--ease-out) both;
@@ -218,66 +183,6 @@ const ContactLinks = styled.div`
     grid-template-columns: 1fr 1fr;
     margin-bottom: var(--spacing-4x);
   }
-`;
-
-const Experience = styled.section`
-  display: flex;
-  flex-direction: column;
-  grid-column: -5 / -1;
-  animation: onload-fade var(--duration-load) var(--ease-out) both;
-  animation-delay: var(--stagger-1);
-
-  @media ${QUERIES.phoneAndBelow} {
-    grid-column: 1 / -1;
-  }
-`;
-
-const TableTitle = styled.p`
-  color: var(--color-text-muted);
-  font-weight: var(--font-weight-normal);
-`;
-
-const Table = styled.table`
-  margin: 8px 0;
-`;
-
-const TableRow = styled.tr`
-  border-bottom: 1px solid var(--color-border);
-  display: grid;
-  grid-template-columns: 0.5fr 1fr 2.5fr;
-  column-gap: 16px;
-  padding: 6px 0;
-  padding-right: 24px;
-  align-items: baseline;
-
-  font-weight: var(--font-weight-normal);
-  color: var(--color-text-muted);
-
-  &:first-of-type {
-    border-top: 1px solid var(--color-border);
-  }
-
-  @media ${QUERIES.phoneAndBelow} {
-    padding-right: 0;
-  }
-`;
-
-const TdYear = styled.td`
-  font-family: var(--font-mono);
-  font-size: var(--font-size-xs);
-`;
-
-const TdCompany = styled.td`
-  color: var(--color-text-base);
-`;
-
-const TdRole = styled.td``;
-
-const ReadCV = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding-right: 12px;
 `;
 
 const Projects = styled.section`
