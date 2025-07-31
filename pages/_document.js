77f14862bc8738706d3a70648150526841b2b9d0
Charlsy Yang <imcharlsy@gmail.com>
2025-07-30 22:35:10 -0700

    Update analytics to track outbound links

--- pages/_document.js
+++ pages/_document.js
@@ -1,19 +1,19 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
-import { ServerStyleSheet } from 'styled-components'
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import { ServerStyleSheet } from "styled-components";
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
-    const sheet = new ServerStyleSheet()
-    const originalRenderPage = ctx.renderPage
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
 
     try {
       ctx.renderPage = () =>
         originalRenderPage({
           enhanceApp: (App) => (props) =>
             sheet.collectStyles(<App {...props} />),
-        })
+        });
 
-      const initialProps = await Document.getInitialProps(ctx)
+      const initialProps = await Document.getInitialProps(ctx);
       return {
         ...initialProps,
         styles: (
@@ -22,9 +22,9 @@ class MyDocument extends Document {
             {sheet.getStyleElement()}
           </>
         ),
-      }
+      };
     } finally {
-      sheet.seal()
+      sheet.seal();
     }
   }
 
@@ -32,22 +32,53 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <link href="https://fastly-cloud.typenetwork.com/projects/7347/fontface.css?64a210d9" rel="stylesheet" type="text/css"/>
-          <link rel="alternate" type="application/rss+xml" title="Blog RSS Feed" href="/feed.xml" />
-          <link rel="icon" href="/favicons/favicon.ico"/>
-          <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#5746AF"/>
-          <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png"/>
-          <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png"/>
-          <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png"/>
-          <script defer data-domain="charlsyang.com" src="https://plausible.io/js/script.js"></script>
+          <link
+            href="https://fastly-cloud.typenetwork.com/projects/7347/fontface.css?64a210d9"
+            rel="stylesheet"
+            type="text/css"
+          />
+          <link
+            rel="alternate"
+            type="application/rss+xml"
+            title="Blog RSS Feed"
+            href="/feed.xml"
+          />
+          <link rel="icon" href="/favicons/favicon.ico" />
+          <link
+            rel="mask-icon"
+            href="/favicons/safari-pinned-tab.svg"
+            color="#5746AF"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/favicons/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicons/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicons/favicon-16x16.png"
+          />
+          <script
+            defer
+            data-domain="charlsyang.com"
+            src="https://plausible.io/js/script.outbound-links.js"
+          ></script>
         </Head>
         <body>
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
 
-export default MyDocument
+export default MyDocument;
